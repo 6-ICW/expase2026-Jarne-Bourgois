f97ce032a7ffccdf68441eaaf40b89ee12be2afe
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2026-03-23 13:57:04 +0100

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import RegistrationForm from "./components/RegistrationForm";
+import UserList from "./components/UserList";
 
 export default function App() {
+  const [gbrnaam, setGbrnaam] = useState([{ id: 1, naam: "Jarne" }]);
+  const GeregistreerdePersonen = ["Karel", "Els", "Piet"];
+
+  const onSubmit = (naam: string) => {
+    const newId = Math.max(gbrnaam.length) + 1;
+    const newGbr = { id: newId, naam };
+    setGbrnaam([...gbrnaam, newGbr]);
+    console.log(gbrnaam);
+  };
 
   return (
-    const GeregistreerdePersonen = ["Karel", "Els", "Piet"];
-    <div>
-      <RegistrationForm />
-      <hr />
-      <UserList />
-    </div >
+    <>
+      <div>
+        <RegistrationForm onRegister={onSubmit} />
+        <hr />
+        <UserList users={gbrnaam} registeredUsers={GeregistreerdePersonen} />
+      </div>
+    </>
   );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,3 +1,30 @@
+interface gebruiker {
+  id: number;
+  naam: string;
+}
+
+interface Props {
+  users: gebruiker[];
+  registeredUsers: string[];
+}
+
 export default function UserList({ users, registeredUsers }: Props) {
-    return <></>
+  return (
+    <>
+      <ul>
+        {users.map((gbr) => (
+          <li
+            key={gbr.id}
+            style={
+              registeredUsers.filter((reg) => reg == gbr.naam).toString() == ""
+                ? { color: "green" }
+                : { color: "rood" }
+            }
+          >
+            {gbr.naam}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
